f982d8a40cc931a7deea095b6fe5e2de426133e7
Fernando Vegas <90532274+Thgear27@users.noreply.github.com>
2024-04-05 22:19:18 -0500

    update to deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 
-import node from "@astrojs/node";
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: deno({
+    mode: "standalone",
+  }),
 });
